5bb2c678ace57562f91477a21ea0a9ccf67328fb
keithroe <keithroe@gmail.com>
2026-02-28 08:22:36 -0700

    daily update

--- rust_scraper/src/maverik.rs
+++ rust_scraper/src/maverik.rs
@@ -23,23 +23,49 @@ use crate::util;
 
 */
 
+/*
+<div class="event-info">
+    <div class="event-date"> March 14, 7:10PM </div>
+    <div class="event-title"> Utah Grizzlies vs. Tulsa Oilers </div>
+    <div class="sub-title"></div>
+    <div class="event-desc"> </div>
+    <div class="event-action">
+        <a href="https://maverikcenter.com/event/utah-grizzlies-vs-tulsa-oilers-2/?event_source=featured-events" class="btn-link">Event Info</a>
+        <a href="https://www.ticketmaster.com/utah-grizzlies-vs-tulsa-oilers-west-valley-city-utah-03-14-2026/event/1E0062F889AA237E" target="blank" class="btn btn-default home_get_tickets" data-type="Sports" data-genre="Hockey" data-subgenre="Minor League" data-organizer="PROMOTED BY VENUE">Get Tickets</a>
+    </div>
+</div>
+*/
+
 pub fn scrape() -> Vec<show::Show> {
     println!("processing maverik center ...");
 
     let mut shows = Vec::new();
-    let html = util::get_html("https://maverikcenter.com/events-tickets/upcoming-events/").unwrap();
+    //let html = util::get_html("https://maverikcenter.com/events-tickets/upcoming-events/").unwrap();
+    let html = util::get_html("https://maverikcenter.com/events/").unwrap();
 
-    for event in html.select(&scraper::Selector::parse("div.each-event").unwrap()) {
-        let data_div_elmt = util::select_single(event, "div.data-info").unwrap();
+    for event in html.select(&scraper::Selector::parse("div.event-info").unwrap()) {
+        let date_div_elmt = util::select_single(event, "div.event-date").unwrap();
+        let date_str = util::get_text(date_div_elmt);
 
-        let date_str = util::get_text(util::select_single(data_div_elmt, "h5").unwrap());
-        let artist_str = util::get_text(util::select_single(data_div_elmt, "h4").unwrap());
-
+        let artist_div_elmt = util::select_single(event, "div.event-title").unwrap();
+        let artist_str = util::get_text(artist_div_elmt);
         if artist_str.contains("Grizzlies") {
             continue;
         }
 
-        let link_elmt = util::select_single(event, "div.buttons > a").unwrap();
+        let action_div_elmt = if let Ok(val) = util::select_single(event, "div.event-action") {
+            val
+        } else {
+            continue;
+        };
+
+        let link_elmt = if let Ok(val) = util::select_single(action_div_elmt, "a.home_get_tickets")
+        {
+            val
+        } else {
+            continue;
+        };
+
         let url_str = link_elmt.attr("href").unwrap().to_string();
 
         let date_tokens: Vec<_> = date_str
